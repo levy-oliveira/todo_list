99a14d43b1319a8cb55144d7e08515bd8f4efc70
levy-oliveira <levy.nascimento.oliveira10@gmail.com>
2024-04-13 18:07:24 -0300

    Limpando branch para o frontend

--- models/list.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type List struct {
-    Id uint
-    Name string
-    Decription string
-    Status bool
-	User_id int
-}
--- models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-    Id uint
-    Name string
-    Login string `gorm:"unique"`
-    Password string
-}
