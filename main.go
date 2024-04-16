8b35044fe4319571ab71a34bfcef8eaa26eea99b
levy-oliveira <levy.nascimento.oliveira10@gmail.com>
2024-04-16 15:17:31 -0300

    correções para conexão com o frontend

--- main.go
+++ main.go
@@ -9,6 +9,19 @@ import (
 func main() {
 	database.Connect()
 	app := fiber.New()
+
+	// Middleware CORS
+    app.Use(func(c *fiber.Ctx) error {
+        c.Set("Access-Control-Allow-Origin", "*") // Permitir solicitações de qualquer origem
+        c.Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS") // Métodos HTTP permitidos
+        c.Set("Access-Control-Allow-Headers", "Content-Type, Authorization") // Headers permitidos
+        if c.Method() == "OPTIONS" {
+            // Responder imediatamente para solicitações OPTIONS
+            return c.SendStatus(fiber.StatusNoContent)
+        }
+        // Avance para o próximo middleware
+        return c.Next()
+    })
 	routes.Setup(app)
 	app.Listen(":3000")
 }
